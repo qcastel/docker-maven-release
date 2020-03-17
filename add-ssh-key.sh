d30068cf25967e0e352448596c31b0b30126e492
Quentin Castel <quentin.castel@yapily.com>
2020-03-17 18:10:54 +0000

    Set StrictHostKeyChecking accept-new

--- add-ssh-key.sh
+++ add-ssh-key.sh
@@ -6,3 +6,6 @@ echo "Do a SSH add with the key under env 'SSH_PRIVATE_KEY'"
 mkdir -p ~/.ssh/
 echo "$SSH_PRIVATE_KEY" | base64 -d > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+
+echo "Set StrictHostKeyChecking accept-new"
+echo -e "Host *\n\tStrictHostKeyChecking accept-new\n\n" > ~/.ssh/config
