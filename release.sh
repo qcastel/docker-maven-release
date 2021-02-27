bfcaba7665b1f8d8c27cc510d900a4ce52d44170
Quentin Castel <quentincastel86@gmail.com>
2021-02-27 17:51:03 +0000

    Fix log

--- release.sh
+++ release.sh
@@ -13,7 +13,6 @@ fi
 
 if [ -d "${M2_HOME_FOLDER}" ]; then
      echo "INFO - M2 folder '${M2_HOME_FOLDER}' not empty. We therefore will beneficy from the CI cache"; 
-     ls -l ${M2_HOME_FOLDER}/.m2; 
 else 
      echo "WARN - No M2 folder '${M2_HOME_FOLDER}' found. We therefore won't beneficy from the CI cache"; 
 fi
